26ad7c2c5399a6ab726e48f83dcbd3141ff7ba82
Chris20008 <range-ch@web.de>
2025-07-04 19:54:22 +0200

    Minor fix connect health

--- lib/util/config.dart
+++ lib/util/config.dart
@@ -207,6 +207,7 @@ class CnConfig extends ChangeNotifier {
       }
       return await cnScreenStatistics.health.hasPermissions(cnScreenStatistics.types)?? false;
     }
+    // await setHealth(true);
     isWaitingForHealthResponse = true;
     await Future.delayed(const Duration(milliseconds: 300), ()async{
       permission = await cnScreenStatistics.health.hasPermissions(cnScreenStatistics.types);
